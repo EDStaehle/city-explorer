40ca353e3b3c6f9a5bde2cd7fedc44b41961d88c
Elias <elias.staehle@gmail.com>
2022-10-24 21:10:44 -0400

    added some stylez

--- src/SelectedCity.js
+++ src/SelectedCity.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import './SelectedCity.css';
 
 class SelectedCity extends React.Component{
   render(){
@@ -10,13 +11,17 @@ class SelectedCity extends React.Component{
         <Form.Group>
           <legend>Explore some cities!!</legend>
           <Form.Control
+          className="form"
           type='text'
           onInput={this.props.handleInput}
           />
-          <img src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_CITYSELECTOR_API_KEY}&center=${this.props.cityData.lat},${this.props.cityData.lon}&zoom=10`} alt=''></img>
         </Form.Group>
-        <Button type="submit">Submit</Button>
+        <Button 
+        className='btn'
+        type="submit">Submit</Button>
+
       </Form>
+          <img className="img" src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_CITYSELECTOR_API_KEY}&center=${this.props.cityData.lat},${this.props.cityData.lon}&zoom=10`} alt=''></img>
       {
         this.props.error? <p>{this.props.errorMessage}</p>: <p>{this.props.cityData.display_name}<br></br> Lon {this.props.cityData.lon}<br></br> Lat {this.props.cityData.lat}</p>
       }
