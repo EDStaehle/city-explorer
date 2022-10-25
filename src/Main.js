efb13290c0120ae5a4a30233c9f61ef614971651
Elias <elias.staehle@gmail.com>
2022-10-24 20:38:14 -0400

    day one lab assignment

---
+++ src/Main.js
@@ -0,0 +1,14 @@
+import React from "react";
+import SelectedCity from './SelectedCity';
+import SelectedMap from './SelectedMap';
+
+
+class Main extends React.Component{
+  render(){
+    return(
+      <main>
+      </main>
+    )
+  }
+}
+export default Main;
---
+++ src/SelectedMap.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class SelectedMap extends React.Component{
+  render(){
+    return(
+      <h2>stuffss</h2>
+    )
+  }
+}
