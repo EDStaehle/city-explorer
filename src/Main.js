0f6bf080f4d1ac9f5e31f6a7900260a8a3a9f141
Elias <elias.staehle@gmail.com>
2022-10-24 20:40:58 -0400

    hot fix netlify deploy issues

--- src/Main.js
+++ src/Main.js
@@ -1,6 +1,5 @@
 import React from "react";
-import SelectedCity from './SelectedCity';
-import SelectedMap from './SelectedMap';
+
 
 
 class Main extends React.Component{
--- src/SelectedMap.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-class SelectedMap extends React.Component{
-  render(){
-    return(
-      <h2>stuffss</h2>
-    )
-  }
-}
